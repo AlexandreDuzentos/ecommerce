4d37a58339fb0f6e859ac5e9d872c14e72871436
AlexandreDuzentos <60664871+AlexandreDuzentos@users.noreply.github.com>
2020-08-20 02:11:33 +0100

    criando classe page e tpl admin

--- index.php
+++ index.php
@@ -3,16 +3,26 @@
 require_once("vendor/autoload.php");
 
 use Hcode\Page;
+use Hcode\PageAdmin;
 
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
+//Rota para a home page
 $app->get('/', function() {
     
 	$page = new Page();
 
 	$page->setTpl("index");
+
+});
+
+$app->get("/admin",function(){
+    
+    $page = new PageAdmin();
+
+    $page->setTpl("index");
 
 });
 
